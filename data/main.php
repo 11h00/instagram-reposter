60d8f934134d1fdcede33f415d99504c26b12f32
11h00 <tpjdog@gmail.com>
2021-08-02 11:43:24 +0200

    added funtion to check if session exist

--- data/main.php
+++ data/main.php
@@ -1,10 +1,32 @@
 <?php
     class mains_functions{
-
+        /**
+         * CheckIfCredentialExist
+         * 
+         * check if the user who sent the request have session to his instagram account
+         * 
+         * /!\ THIS DO NO CHECK IF THE SESSION IS VALID
+         * @see check_session() to check if the session is valid
+         * 
+         * @return bool true or false
+         * 
+         * @author mathias777
+         */
+        public function CheckIfCredentialExist(){
+            if ($_SESSION){
+                if (isset($_SESSION["sessionID"]) && isset($_SESSION["csrftoken"])){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
     }
 
     //after declared the main function, require the other ones.
-    
+
     require_once "./utils.php";
     require_once "./constant.php";
 ?>
