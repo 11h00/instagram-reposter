4ee206ef913ba0a55013d4b838c27721f87d4d16
11h00 <tpjdog@gmail.com>
2021-08-02 10:34:51 +0200

    added check login on start

---
+++ js/login.js
@@ -0,0 +1,9 @@
+let login_request = new XMLHttpRequest()
+
+document.onreadystatechange = function(){
+    if (this.readyState = "complete"){
+        fetch("./data/check_if_valid/", {
+            ""
+        })
+    }
+}
