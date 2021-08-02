e301ebc89b640846bdb6c935ca8d47617b04a6f9
11h00 <tpjdog@gmail.com>
2021-08-02 12:00:09 +0200

    added TestIgFeed

--- data/main.php
+++ data/main.php
@@ -1,5 +1,29 @@
 <?php
+    /**
+     * mains_functions
+     * 
+     * those function should not be edited at all.
+     * its used to get feed and also check if user can post, is logged (no 302) ect..
+     * 
+     */
     class mains_functions{
+
+        protected function TestIgFeed() : int{
+            $feed = curl_init("https://i.instagram.com/api/v1/feed/reels_tray/");
+            curl_setopt_array($feed, [
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_HTTPHEADER => [
+                    "accept: */*",
+                    "accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "origin: https://www.instagram.com",
+                    "referer: https://www.instagram.com/",
+                    "user-agent: " . ""
+                ]
+            ]);
+            curl_exec($feed);
+            return 
+        }
+
         /**
          * CheckIfCredentialExist
          * 
@@ -12,7 +36,8 @@
          * 
          * @author mathias777
          */
-        public function CheckIfCredentialExist(){
+
+        public function CheckIfCredentialExist(): bool{
             if ($_SESSION){
                 if (isset($_SESSION["sessionID"]) && isset($_SESSION["csrftoken"])){
                     return true;
@@ -23,7 +48,16 @@
                 return false;
             }
         }
+
+        public function check_session(){
+            if ($this->CheckIfCredentialExist()){
+                
+            }else{
+                return false;
+            }
+        }
     }
+    
 
     //after declared the main function, require the other ones.
 
