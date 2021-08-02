bca11cffdc1892400a49081d18f4042d107c9863
11h00 <tpjdog@gmail.com>
2021-08-02 10:59:24 +0200

    added functions and comments

--- data/main.php
+++ data/main.php
@@ -2,6 +2,9 @@
     class mains_functions{
 
     }
+
+    //after declared the main function, require the other ones.
+    
     require_once "./utils.php";
     require_once "./constant.php";
 ?>
--- data/utils.php
+++ data/utils.php
@@ -2,23 +2,32 @@
 
 class utiles extends mains_functions{
     /**
+     * GenerateUUID
      * 
+     * generate a UUID version 4 
+     * ex: 
+     * @return string the uuid as string 
+     * @param bool if you dont want to keep the dashes(-)
      */
-    public function generateUUID($keepDashes = true) {
-		$uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
-		return $keepDashes ? $uuid : str_replace('-', '', $uuid);
+    public function generateUUID($keepDashes = true) : string{
+      $uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+      return $keepDashes ? $uuid : str_replace('-', '', $uuid);
     }
     /**
-     *
+     * GenerateRandomString
+     * 
+     * generate a random string from $charactersLength
+     * 
+     * @param int 
      */
-    public function femerla($length) {
-		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		$charactersLength = strlen($characters);
-		$randomString = '';
-		for ($i = 0; $i < $length; $i++) {
-			$randomString .= $characters[rand(0, $charactersLength - 1)];
-		}
-		return $randomString;
+    public function GenerateRandomString(int $length) : string{
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charactersLength = strlen($characters);
+      $randomString = '';
+      for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+      }
+      return $randomString;
     }
     /**
      * 
