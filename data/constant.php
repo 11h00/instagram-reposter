e0bd3aa529eb395c867738f758bb68c1378c657f
11h00 <tpjdog@gmail.com>
2021-08-02 10:41:39 +0200

    added base instagram function

--- data/constant.php
+++ data/constant.php
@@ -1,4 +1,10 @@
 <?php
-    const PUBLIC_API = "";
-    
+    class constants extends mains_functions{
+        const PUBLIC_API = "";
+        const PRIVATE_API = "";
+
+        CONST TYPE_ALBUM_INT = 8;
+        CONST TYPE_FEED_INT = 1;
+        CONST TYPE_IGTV_INT = 8;
+    }
 ?>
---
+++ data/main.php
@@ -0,0 +1,5 @@
+<?php
+    class mains_functions{
+        
+    }
+?>
