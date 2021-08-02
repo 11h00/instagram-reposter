0d9a5c679474a8d03f7503e9e14405dcf01ff5ca
11h00 <tpjdog@gmail.com>
2021-08-02 10:45:16 +0200

    radom text generator

--- data/utils.php
+++ data/utils.php
@@ -2,6 +2,15 @@
     function generateUUID($keepDashes = true) {
 		$uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
 		return $keepDashes ? $uuid : str_replace('-', '', $uuid);
-}
+    }
+    function femerla($length) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+    }
 
 ?>
