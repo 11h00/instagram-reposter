1e1a89c45fbe2661b9e5b32c5ec54f0c9327c738
11h00 <tpjdog@gmail.com>
2021-08-02 13:48:14 +0200

    calling parent::usergent on function

--- data/utils.php
+++ data/utils.php
@@ -67,7 +67,7 @@ class utiles extends mains_functions{
      */
 
     static function GetUserAgent(){
-      if (count(parent::USERAGENT) > 0){
+      if (count(parent::$UserAgent) > 0){
 
       }else{
         throw new Exception("No user agents");
